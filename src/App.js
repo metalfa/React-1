c1616c08b617f1deb4ef9304cc1efda9d280bdb0
metalfa <bensassi.faysel@gmail.com>
2022-04-13 22:37:39 +0100

    dang

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import TodoList from './TodoList'
+//import uuidv4 from 'uuid/v4'
+
+const Local_Storage_Key = 'todoApp.todos'
 
 function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
+
+useEffect(() => {
+  const storedTodos = JSON.parse(localStorage.getItem(Local_Storage_Key))
+if (storedTodos) setTodos(storedTodos)
+}, [])
+
+useEffect(() => {
+localStorage.setItem(Local_Storage_Key, JSON.stringify(todos))}, [todos])
+
+function toggleTodo(id){
+  const newTodos = [... todos]
+  const todo = newTodos.find(todo => todo.id === id) todo.complete = !todo.complete
+  setTodos(newTodos)
+}
 
  function handleAddTodo(e) {
 const name = todoNameRef.current.value
@@ -16,7 +33,7 @@ todoNameRef.current.value = null
   }
   return (
     <>
-    <TodoList todos={todos} />    
+    <TodoList todos={todos} toggleTodo ={toggleTodo} />    
     <input ref={todoNameRef} type="text" />
     <button onClick={handleAddTodo}> Add to do</button>
     <button> Clear to do</button>
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Todo from './Todo'
 
 
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, toggleTodo }) {
   return (
       todos.map(todo => {
 return <Todo key ={todo.id} todo={todo} />
