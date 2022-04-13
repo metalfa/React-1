ae0e68a6737dd60bd0a66b4fc9d913db1f608bdb
metalfa <bensassi.faysel@gmail.com>
2022-04-13 20:10:42 +0100

    rect-1-commit

---
+++ src/ToDoList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ToDoList() {
+  return (
+      <>
+    <div>Hey Sexy</div>
+    <input type="text" />
+    <button> Add to do</button>
+    <button> Clear to do</button>
+    <div>0 left to do</div>
+
+    </>
+  )
+}
